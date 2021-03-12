55d8db7de1314998242e478c5f6f2c7b3369d177
Boon Elschenbroich <mail@boon-elschenbroich.de>
2021-03-12 10:50:50 +0100

    small cleanups

--- src/App.js
+++ src/App.js
@@ -110,19 +110,25 @@ const Row = ({ row, index, moveRow }) => {
       }
       const dragIndex = item.index
       const hoverIndex = index
+
       // Don't replace items with themselves
       if (dragIndex === hoverIndex) {
         return
       }
+
       // Determine rectangle on screen
       const hoverBoundingRect = dndRef.current.getBoundingClientRect()
+
       // Get vertical middle
       const hoverMiddleY =
           (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
       // Determine mouse position
       const clientOffset = monitor.getClientOffset()
+
       // Get pixels to the top
       const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
       // Only perform the move when the mouse has crossed half of the items height
       // When dragging downwards, only move when the cursor is below 50%
       // When dragging upwards, only move when the cursor is above 50%
@@ -134,8 +140,10 @@ const Row = ({ row, index, moveRow }) => {
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
         return
       }
+
       // Time to actually perform the action
       moveRow(dragIndex, hoverIndex)
+
       // Note: we're mutating the monitor item here!
       // Generally it's better to avoid mutations,
       // but it's good here for the sake of performance
